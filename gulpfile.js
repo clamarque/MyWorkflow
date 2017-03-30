c0afa45a4a72d78aa9b9cd311ae3e0d9c568c7cc
Clamarque <chabox59@gmail.com>
2017-03-30 10:04:06 +0200

    Update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,78 @@
-var gulp = require ('gulp');
-var sass = require ('gulp-sass');
-var please = require ('gulp-pleeease');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
+// Requis
+var gulp = require('gulp');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+const imagemin = require('gulp-imagemin');
 
+// Include plugins
+var plugins = require('gulp-load-plugins')();
+
+// Path
+var source ='./assets'; 
+var destination = './public';
+var app = "workflow_";
+const repo = "https://github.com/clamarque/workflow_.git"; 
+
+// Checking exists folders (optional)
+if (!fs.existsSync(source)){
+    mkdirp('./assets/scss/', function(err){
+        console.log(err)
+    })
+}
+else if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination)
+}
+
+//  Task "BUILD"
 gulp.task('sass', function () {
-    gulp.src(['scss/*.scss', '!scss/mixins.scss'])
-        .pipe(plumber())
-        .pipe(sass({errLogToConsole: true}))
-        .pipe(please({
-    optimizers: {
-        "autoprefixer": { "browsers": ["Firefox 25", "Chrome 21", "Android 4"]}
-    }}))
-        .pipe(gulp.dest('./css'))
-        .pipe(notify({message: 'SASS task complete'}));
+    return gulp.src([ source + '/scss/*.scss', '!scss/mixins.scss'])
+        .pipe(plugins.plumber())
+        .pipe(plugins.sass({ errLogToConsole: true }))
+        .pipe(plugins.csscomb())
+        .pipe(plugins.cssbeautify({indent: ' '}))
+       /* .pipe(plugins.uncss({
+            html: []
+        }))
+        */
+        .pipe(gulp.dest(destination))
+        .pipe(plugins.notify({message: 'SASS task complete' }));
 });
 
-gulp.task('default',['watch']);
+gulp.task('optimization', function () {
+    return gulp.src(source + '/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(destination))
+        .pipe(plugins.notify({message: 'Optimized images'}))
+})
 
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass']).on('change', function(event){
-        console.log('Le fichier ' + event.path + ' a ete modifie');
+// Task "MINIFY"
+gulp.task('minify', function () {
+    setTimeout(function(){
+    return gulp.src(destination + '/*.css')
+        .pipe(plugins.plumber())
+        .pipe(plugins.pleeease({
+            optimizers: {
+                "autoprefixer": { "browsers": ["Firefox 25", "Chrome 21", "Android 4"] }
+            }
+        }))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(destination))
+        .pipe(plugins.notify({message: 'Minified files'}))
+    }, 10000)
+});
+
+// Task "build"
+gulp.task('build', ['sass']);
+
+// Task "prod"
+gulp.task('prod', ['build', 'optimization', 'minify']);
+
+gulp.task('watch', function () {
+    gulp.watch(source + '/scss/*.scss', ['sass']).on('change', function (event) {
+        console.log('File ' + event.path + ' was been updated');
     })
 });
+
+gulp.task('default', ['watch']);
